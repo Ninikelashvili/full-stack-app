2c3272235e297bfe205fbf76b060d75173e57a66
Nini Ninikelashvili <66223574+Ninikelashvili@users.noreply.github.com>
2023-04-30 15:08:58 +0400

    connected mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware.js')
+const color = require('colors')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 6000;
 
 
-
+connectDB()
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

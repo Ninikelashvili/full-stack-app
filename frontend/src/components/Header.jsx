2136361de70af6b781a5f4e076763c546dfac46e
Nini Ninikelashvili <66223574+Ninikelashvili@users.noreply.github.com>
2023-04-30 23:36:15 +0400

    Frontend authentication

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,46 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
   return (
     <HeaderWrapper>
       <div>
         <Link to="/">TodoSetter</Link>
       </div>
       <ul>
-        <li>
-          <Link to="/login">
-            <FaSignInAlt />
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <FaUser />
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <button onClick={onLogout}>
+              <FaSignOutAlt />
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt />
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser />
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </HeaderWrapper>
   );
@@ -37,6 +59,11 @@ const HeaderWrapper = styled.header`
     flex-direction: row;
     align-items: center;
     gap: 0 20px;
+    button {
+      border: none;
+      outline: none;
+      background-color: transparent;
+    }
     svg {
       color: black;
       font-size: 20px;
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className="loadingSpinnerContainer">
+      <div className="loadigSpinner"></div>
+    </div>
+  );
+};

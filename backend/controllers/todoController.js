b0891c50880337b0dbdfffddcf66cb89fcea0876
Nini Ninikelashvili <66223574+Ninikelashvili@users.noreply.github.com>
2023-04-30 18:05:26 +0400

    initial REST API for todos

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,40 +1,80 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require("express-async-handler");
+const Todo = require("../models/todoModel");
 //@desc Get Todos
 //@route GET/api/todos
 //@access Private
 const getTodos = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get Todos'})
-})
+  const todos = await Todo.find();
+  res.status(200).json(todos);
+});
 
 //@desc Set Todos
 //@route Post/api/todos
 //@access Private
-const setTodos = asyncHandler (async (req, res) => {
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('Please add a text field')
-    }
-    res.status(200).json({message: 'Set Todo'})
-})
+const setTodo = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text fields");
+  }
+
+  const todo = await Todo.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(todo);
+});
 
 //@desc Update Todo
 //@route PUT/api/todos/:id
 //@access Private
-const updateTodo = asyncHandler ( async (req, res) => {
-    res.status(200).json({message: `Update Todo ${req.params.id}`})
-})
+const updateTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) {
+    res.status(400);
+    throw new Error("Todo not found");
+  }
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedTodo);
+});
 
 //@desc Delete Todo
 //@route DELETE/api/todos/:id
 //@access Private
-const deleteTodo = asyncHandler (async (req, res) => {
-    res.status(200).json({message: `Delete Todo ${req.params.id}`})
-})
+const deleteTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) {
+    res.status(400);
+    throw new Error("Todo not found");
+  }
 
+  await todo.deleteOne();
+  res.status(200).json({ id: req.params.id });
+
+  //   const todo = await Todo.findById(req.params.id);
+  //   if (!todo) {
+  //     res.status(400);
+  //     throw new Error("Todo not found");
+  //   }
+  //   //check for user
+  //   if (!req.user) {
+  //     res.status(401);
+  //     throw new Error("User not found");
+  //   }
+  //   //make sure the logged in user matches the togo user
+  //   if (todo.user.toString() !== req.user.id) {
+  //     res.status(401);
+  //     throw new Error("User not authorized");
+  //   }
+  //   await todo.deleteOne();
+  //   res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
-    getTodos,
-    setTodos,
-    updateTodo,
-    deleteTodo
-}
+  getTodos,
+  setTodo,
+  updateTodo,
+  deleteTodo,
+};
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const {getTodos, updateTodo, setTodos, deleteTodo} = require("../controllers/todoController");
+const express = require("express");
+const router = express.Router();
+const {
+  getTodos,
+  updateTodo,
+  setTodo,
+  deleteTodo,
+} = require("../controllers/todoController");
 
+router.route("/").get(getTodos).post(setTodo);
+router.route("/:id").delete(deleteTodo).put(updateTodo);
 
-
-router.route('/').get(getTodos).post(setTodos)
-router.route('/:id').delete(deleteTodo).put(updateTodo)
-
-module.exports = router
+module.exports = router;
